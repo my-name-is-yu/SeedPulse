915235b6d579cfebeb76728de3efc840e20a8c32
my-name-is-yu <yuyoshimuta1217@gmail.com>
2026-04-06 22:32:32 +0900

    feat(executor): add barrel export for executor module

---
+++ src/runtime/executor/index.ts
@@ -0,0 +1,2 @@
+export { GoalWorker, type GoalWorkerConfig, type WorkerResult, type WorkerStatus } from './goal-worker.js';
+export { LoopSupervisor, type SupervisorConfig, type SupervisorState } from './loop-supervisor.js';
